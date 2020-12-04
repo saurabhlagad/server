ff8d5801d588fdc1a07be4622bd029ccca82b1b7
Aniket2703 <aniketnimbalkar270320@gmail.com>
2020-12-04 11:41:57 +0530

    Path change

--- site/db.js
+++ site/db.js
@@ -1,9 +1,10 @@
 const mysql=require('mysql2')
 const pool=mysql.createPool({
     host:'localhost',
+    port:3309,
     user:'root',
-    password:'abhith@1234',
-    database:'car_rental',
+    password:'@Aniket270320',
+    database:'car_rental_sy',
     connectionLimit:20,
     waitForConnections:true,
     queueLimit:0
